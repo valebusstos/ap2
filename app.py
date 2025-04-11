84c55a53acce78c4fc122c087938786fd5b26f0c
Sala 203 <sala203@ujaverianas.loc>
2025-04-11 14:27:42 -0500

    k

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ with tab1:
 
     # educ - grafico de barras
     tab_freq = ran['educ'].value_counts().sort_index()
-    ax[0].bar(tab_freq.index, tab_freq.values)
+    ax[0].bar(tab_freq.index, tab_freq.values, color = "purple")
 
     # sexo - histograma
-    ax[1].hist(ran['edad'], bins = 30)
+    ax[1].hist(ran['edad'], bins = 30, color = "yellow")
 
     # wage
-    ax[2].hist(ran['wage'], bins = 40)
+    ax[2].hist(ran['wage'], bins = 40, color = "pink")
 
     st.pyplot(fig)
 
@@ -30,10 +30,10 @@ with tab1:
     fig, ax = plt.subplots(1, 2, figsize = (10, 4))
 
     # educ vs. wage
-    ax[0].scatter(ran['educ'], ran['wage'])
+    ax[0].scatter(ran['educ'], ran['wage'], color = "green")
 
     # edad vs. wage
-    ax[1].scatter(ran['edad'], ran['wage']) 
+    ax[1].scatter(ran['edad'], ran['wage'], color = "blue") 
 
     st.pyplot(fig)
 
@@ -42,5 +42,5 @@ with tab2:
            path = [px.Constant("PGN"),
                    "Nombre Sector",
                    "Tipo de gasto"],
-           values = 'Valor')
+           values = 'Valor', color = "Nombre Sector", color_discrete_sequence=px.colors.qualitative.Pastel)
     st.plotly_chart(fig)
